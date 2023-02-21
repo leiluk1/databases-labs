50154a028c6a522a48c30e42ec192bfe4c3d964e
leiluk1 <88292173+leiluk1@users.noreply.github.com>
2023-02-21 17:48:42 +0300

    Update ex1.sql

--- week5/ex1.sql
+++ week5/ex1.sql
@@ -1,3 +1,47 @@
+--1
 SELECT title
 FROM course
-WHERE dept_name = 'Comp. Sci.'
+WHERE dept_name = 'Comp. Sci.' AND credits = 3;
+
+--2
+SELECT DISTINCT course.course_id, course.title
+FROM takes 
+JOIN course ON course.course_id = takes.course_id
+WHERE takes.id = '45678';
+
+--3
+SELECT course.course_id, course.title, SUM(course.credits) AS tot_credits
+FROM takes 
+JOIN course ON course.course_id = takes.course_id
+WHERE takes.id = '45678'
+GROUP BY course.course_id, course.title;
+
+--4
+SELECT student.id, SUM(course.credits) AS tot_credits
+FROM student
+INNER JOIN takes ON takes.id = student.id
+INNER JOIN course ON course.course_id = takes.course_id
+GROUP BY student.id;
+
+--5
+SELECT DISTINCT student.name
+FROM student 
+JOIN takes ON takes.ID = student.ID
+JOIN course ON takes.course_id = course.course_id
+WHERE course.dept_name = 'Comp. Sci.';
+
+--6
+SELECT instructor.id
+FROM instructor
+LEFT JOIN teaches ON instructor.id = teaches.id
+WHERE teaches.id IS NULL;
+
+--7
+SELECT instructor.id, instructor.name
+FROM instructor
+LEFT JOIN teaches ON instructor.id = teaches.id
+WHERE NOT EXISTS (
+  SELECT 1
+  FROM teaches
+  WHERE teaches.id = instructor.id
+);
